df30410aca413aac212f7cb4ae92c7990a33f82d
Trees <treesbot@gmail.com>
2015-10-17 11:45:33 -0400

    Humanizer - Small Update

--- Humanizer/Program.cs
+++ Humanizer/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using LeagueSharp;
 using LeagueSharp.Common;
+using SharpDX;
 
 #endregion
 
@@ -16,6 +17,10 @@ namespace Humanizer
         public static Obj_AI_Base Player = ObjectManager.Player;
         public static List<string> SpellList = new List<string> { "Q", "W", "E", "R" };
         public static List<float> LastCast = new List<float> { 0, 0, 0, 0 };
+        public static Render.Text BlockedMovement;
+        public static Render.Text BlockedSpells;
+        public static int BlockedSpellCount;
+        public static int BlockedMoveCount;
 
         private static void Main(string[] args)
         {
@@ -37,12 +42,28 @@ namespace Humanizer
                 menu.AddItem(new MenuItem("MaxDelay" + i, "Maximum Delay", true).SetValue(new Slider(200, 100, 400)));
             }
 
+            spells.AddItem(new MenuItem("DrawSpells", "Draw Blocked Spell Count").SetValue(true));
+
             var move = Menu.AddSubMenu(new Menu("Movement", "Movement"));
             move.AddItem(new MenuItem("MovementEnabled", "Enabled").SetValue(true));
             move.AddItem(new MenuItem("MinDelay", "Minimum Delay")).SetValue(new Slider(80));
             move.AddItem(new MenuItem("MaxDelay", "Maximum Delay")).SetValue(new Slider(200, 100, 400));
+            move.AddItem(new MenuItem("DrawMove", "Draw Blocked Movement Count").SetValue(true));
 
             Menu.AddToMainMenu();
+            Console.WriteLine("{0} {1}", Drawing.Width, Drawing.Height);
+            BlockedSpells = new Render.Text(
+                "Blocked Spells: ", Drawing.Width - 200, Drawing.Height - 600, 28, Color.Green);
+            BlockedSpells.VisibleCondition += sender => Menu.Item("DrawSpells").IsActive();
+            BlockedSpells.TextUpdate += () => "Blocked Spells: " + BlockedSpellCount;
+            BlockedSpells.Add();
+
+            BlockedMovement = new Render.Text(
+                "Blocked Move: ", Drawing.Width - 200, Drawing.Height - 625, 28, Color.Green);
+            BlockedMovement.VisibleCondition += sender => Menu.Item("DrawMove").IsActive();
+            BlockedMovement.TextUpdate += () => "Blocked Move: " + BlockedMoveCount;
+            BlockedMovement.Add();
+
 
             Obj_AI_Base.OnIssueOrder += Obj_AI_Base_OnIssueOrder;
             Spellbook.OnCastSpell += Spellbook_OnCastSpell;
@@ -60,10 +81,11 @@ namespace Humanizer
 
             var min = Menu.Item("MinDelay" + spell, true).GetValue<Slider>().Value;
             var max = Menu.Item("MaxDelay" + spell, true).GetValue<Slider>().Value;
-            var delay = min > max ? min : WeightedRandom.Next(min, max);
+            var delay = min >= max ? min : WeightedRandom.Next(min, max);
 
             if (Utils.TickCount - LastCast[spell] < delay)
             {
+                BlockedSpellCount++;
                 args.Process = false;
                 return;
             }
@@ -85,6 +107,7 @@ namespace Humanizer
 
             if (Utils.TickCount - LastMove < delay)
             {
+                BlockedMoveCount++;
                 args.Process = false;
                 return;
             }
