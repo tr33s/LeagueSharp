a8294e3b2909ffda3913dcd507ca664c4437b249
Trees <treesbot@gmail.com>
2015-10-25 01:01:55 -0400

    TreeLib - Sound Object

---
+++ TreeLib/Objects/SoundObject.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Media;
+using LeagueSharp.Common;
+
+namespace TreeLib.Objects
+{
+    public class SoundObject
+    {
+        public static float LastPlayed;
+        private static SoundPlayer _sound;
+
+        public SoundObject(Stream sound)
+        {
+            LastPlayed = 0;
+            _sound = new SoundPlayer(sound);
+        }
+
+        public void Play()
+        {
+            if (Utils.TickCount - LastPlayed < 1500)
+            {
+                return;
+            }
+            _sound.Play();
+            LastPlayed = Utils.TickCount;
+        }
+    }
+}
