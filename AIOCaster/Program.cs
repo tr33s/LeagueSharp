945cda99450b86986cdc2caa3ff73e0160b18fb1
Trees <treesbot@gmail.com>
2015-12-28 09:56:07 -0500

    AIOCaster - Fix Champs with multiple spells per slot

--- AIOCaster/Program.cs
+++ AIOCaster/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
+using LeagueSharp.SDK.Core.Events;
 using LeagueSharp.SDK.Core.Wrappers.SpellDatabase;
 
 namespace AIOCaster
 {
     internal class Program
     {
-        public static Dictionary<SpellSlot, Spell> Spells = new Dictionary<SpellSlot, Spell>();
+        public static List<SpellDatabaseEntry> Spells = new List<SpellDatabaseEntry>();
         public static Menu Menu;
         public static Orbwalking.Orbwalker Orbwalker;
 
@@ -20,10 +21,11 @@ namespace AIOCaster
 
         private static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            Load.OnLoad += Game_OnGameLoad;
+            //CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        private static void Game_OnGameLoad(EventArgs args)
+        private static void Game_OnGameLoad(object sender, EventArgs e)
         {
             if (!SpellDatabase.Spells.Any(s => s.ChampionName.Equals(Player.ChampionName)))
             {
@@ -39,22 +41,20 @@ namespace AIOCaster
                 SpellDatabase.Spells.Where(s => s.ChampionName.Equals(Player.ChampionName) && s.SpellType.IsSkillShot())
                 )
             {
-                var s = new Spell(spell.Slot, spell.Range);
-                var collision = spell.CollisionObjects.Length > 1;
-                var type = spell.SpellType.GetSkillshotType();
-
-                s.SetSkillshot(spell.Delay, spell.Width, spell.MissileSpeed, collision, type);
-                Spells.Add(s.Slot, s);
+                Spells.Add(spell);
             }
 
             var spellMenu = Menu.AddSubMenu(new Menu("Spells", "Spells"));
+            var addedSpells = new List<SpellSlot>();
 
-            foreach (var spell in Spells)
+            foreach (var spell in Spells.Where(s => !addedSpells.Contains(s.Slot)))
             {
-                var s = spell.Key.ToString();
+                var s = spell.Slot.ToString();
+                Console.WriteLine(s);
                 var menu = spellMenu.AddSubMenu(new Menu(s, s));
                 menu.AddItem(new MenuItem(s + "Combo", "Use in Combo", true).SetValue(true));
                 menu.AddItem(new MenuItem(s + "Mixed", "Use in Harass", true).SetValue(true));
+                addedSpells.Add(spell.Slot);
             }
 
             Menu.AddToMainMenu();
@@ -72,16 +72,24 @@ namespace AIOCaster
 
             var mode = Orbwalker.ActiveMode.ToString();
 
-            foreach (var spell in Spells.Where(s => s.Value.IsReady()))
+            foreach (var spell in Spells.Where(s => Player.Spellbook.GetSpell(s.Slot).IsReady()))
             {
-                var active = Menu.Item(spell.Key + mode, true) != null && Menu.Item(spell.Key + mode, true).IsActive();
-
-                if (!active)
+                if (!Menu.Item(spell.Slot + mode, true).IsActive())
                 {
-                    return;
+                    continue;
                 }
 
-                spell.Value.CastOnBestTarget();
+                if (!Player.Spellbook.GetSpell(spell.Slot).Name.ToLower().Equals(spell.SpellName.ToLower()))
+                {
+                    continue;
+                }
+
+                var s = new Spell(spell.Slot, spell.Range);
+                var collision = spell.CollisionObjects.Length > 1;
+                var type = spell.SpellType.GetSkillshotType();
+
+                s.SetSkillshot(spell.Delay, spell.Width, spell.MissileSpeed, collision, type);
+                s.CastOnBestTarget();
             }
         }
     }
