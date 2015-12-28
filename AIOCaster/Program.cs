d01635591217c0873e2975c3dbf51ee1f81880e4
Trees <treesbot@gmail.com>
2015-12-28 06:30:53 -0500

    AIOCaster - Initial Commit

---
+++ AIOCaster/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using LeagueSharp.SDK.Core.Wrappers.SpellDatabase;
+
+namespace AIOCaster
+{
+    internal class Program
+    {
+        public static Dictionary<SpellSlot, Spell> Spells = new Dictionary<SpellSlot, Spell>();
+        public static Menu Menu;
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        public static Obj_AI_Hero Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (!SpellDatabase.Spells.Any(s => s.ChampionName.Equals(Player.ChampionName)))
+            {
+                return;
+            }
+
+            Menu = new Menu("AIOCaster", "AIOCaster", true);
+
+            var orbMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            Orbwalker = new Orbwalking.Orbwalker(orbMenu);
+
+            foreach (var spell in
+                SpellDatabase.Spells.Where(s => s.ChampionName.Equals(Player.ChampionName) && s.SpellType.IsSkillShot())
+                )
+            {
+                var s = new Spell(spell.Slot, spell.Range);
+                var collision = spell.CollisionObjects.Length > 1;
+                var type = spell.SpellType.GetSkillshotType();
+
+                s.SetSkillshot(spell.Delay, spell.Width, spell.MissileSpeed, collision, type);
+                Spells.Add(s.Slot, s);
+            }
+
+            var spellMenu = Menu.AddSubMenu(new Menu("Spells", "Spells"));
+
+            foreach (var spell in Spells)
+            {
+                var s = spell.Key.ToString();
+                var menu = spellMenu.AddSubMenu(new Menu(s, s));
+                menu.AddItem(new MenuItem(s + "Combo", "Use in Combo").SetValue(true));
+                menu.AddItem(new MenuItem(s + "Mixed", "Use in Harass").SetValue(true));
+            }
+
+            Menu.AddToMainMenu();
+
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (Player.IsDead || !Orbwalker.ActiveMode.IsComboMode())
+            {
+                return;
+            }
+
+            var mode = Orbwalker.ActiveMode.ToString();
+
+            foreach (var spell in Spells.Where(s => s.Value.IsReady()))
+            {
+                var active = Menu.Item(spell.Key + mode) != null && Menu.Item(spell.Key + mode).IsActive();
+
+                if (!active)
+                {
+                    return;
+                }
+
+                spell.Value.CastOnBestTarget();
+            }
+        }
+    }
+}
---
+++ AIOCaster/Utility.cs
@@ -0,0 +1,38 @@
+﻿using LeagueSharp.Common;
+using LeagueSharp.SDK.Core.Enumerations;
+using SkillshotType = LeagueSharp.Common.SkillshotType;
+
+namespace AIOCaster
+{
+    internal static class Utility
+    {
+        public static bool IsSkillShot(this SpellType type)
+        {
+            return type.Equals(SpellType.SkillshotCircle) || type.Equals(SpellType.SkillshotCone) ||
+                   type.Equals(SpellType.SkillshotMissileCone) || type.Equals(SpellType.SkillshotLine) ||
+                   type.Equals(SpellType.SkillshotMissileLine);
+        }
+
+        public static SkillshotType GetSkillshotType(this SpellType type)
+        {
+            switch (type)
+            {
+                case SpellType.SkillshotCircle:
+                    return SkillshotType.SkillshotCircle;
+                case SpellType.SkillshotCone:
+                case SpellType.SkillshotMissileCone:
+                    return SkillshotType.SkillshotCone;
+                case SpellType.SkillshotMissileLine:
+                case SpellType.SkillshotLine:
+                    return SkillshotType.SkillshotLine;
+            }
+
+            return SkillshotType.SkillshotLine;
+        }
+
+        public static bool IsComboMode(this Orbwalking.OrbwalkingMode mode)
+        {
+            return mode.Equals(Orbwalking.OrbwalkingMode.Combo) || mode.Equals(Orbwalking.OrbwalkingMode.Mixed);
+        }
+    }
+}
