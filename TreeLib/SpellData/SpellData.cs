caca22edbef51b67f9ae21b11b08fff88628ba7c
Trees <treesbot@gmail.com>
2016-11-26 01:55:46 -0500

    ???

--- TreeLib/SpellData/SpellData.cs
+++ TreeLib/SpellData/SpellData.cs
@@ -1,122 +1,121 @@
-﻿using LeagueSharp;
+﻿#region
+
+using LeagueSharp;
+
+#endregion
 
 namespace TreeLib.SpellData
 {
     public class SpellData
     {
-        #region Fields
-
         public bool AddHitbox;
-
         public bool CanBeRemoved = false;
-
         public bool Centered;
-
-        public CollisionObjectTypes[] CollisionObjects = { };
-
         public string ChampionName;
-
+        public CollisionObjectTypes[] CollisionObjects = { };
         public int DangerValue;
-
         public int Delay;
-
         public bool DisabledByDefault = false;
-
         public bool DisableFowDetection = false;
-
         public bool DontAddExtraDuration;
-
-        public bool DontCross = false;
-
         public bool DontCheckForDuplicates = false;
-
+        public bool DontCross = false;
         public bool DontRemove = false;
-
         public int ExtraDuration;
-
         public string[] ExtraMissileNames = { };
-
         public int ExtraRange = -1;
-
         public string[] ExtraSpellNames = { };
-
         public bool FixedRange;
-
-        public bool FollowCaster = false;
-
         public bool ForceRemove = false;
-
+        public bool FollowCaster = false;
         public string FromObject = "";
-
         public string[] FromObjects = { };
-
         public int Id = -1;
-
         public bool Invert;
-
         public bool IsDangerous = false;
-
         public int MissileAccel = 0;
-
         public bool MissileDelayed;
-
         public bool MissileFollowsUnit;
-
         public int MissileMaxSpeed;
-
         public int MissileMinSpeed;
-
         public int MissileSpeed;
-
         public string MissileSpellName = "";
-
+        public EarlyObjects[] EarlyEvade;
         public float MultipleAngle;
-
         public int MultipleNumber = -1;
-
         public int RingRadius;
-
+        public string SourceObjectName = "";
         public SpellSlot Slot;
-
         public string SpellName;
-
         public bool TakeClosestPath = false;
-
         public string ToggleParticleName = "";
-
         public SkillShotType Type;
+        private int _radius;
+        private int _range;
+        
+        public SpellData() { }
 
-        #endregion
+        public SpellData(string championName,
+            string spellName,
+            SpellSlot slot,
+            SkillShotType type,
+            int delay,
+            int range,
+            int radius,
+            int missileSpeed,
+            bool addHitbox,
+            bool fixedRange,
+            int defaultDangerValue)
+        {
+            ChampionName = championName;
+            SpellName = spellName;
+            Slot = slot;
+            Type = type;
+            Delay = delay;
+            Range = range;
+            _radius = radius;
+            MissileSpeed = missileSpeed;
+            AddHitbox = addHitbox;
+            FixedRange = fixedRange;
+            DangerValue = defaultDangerValue;
+        }
 
-        #region Public Properties
+        public string MenuItemName
+        {
+            get { return ChampionName + " - " + SpellName; }
+        }
 
         public int Radius
         {
             get
             {
-                return !AddHitbox
-                    ? RawRadius + Config.SkillShotsExtraRadius
-                    : Config.SkillShotsExtraRadius + RawRadius + (int) ObjectManager.Player.BoundingRadius;
+                return (!AddHitbox)
+                    ? _radius + Config.SkillShotsExtraRadius
+                    : Config.SkillShotsExtraRadius + _radius + (int) ObjectManager.Player.BoundingRadius;
             }
-            set { RawRadius = value; }
+            set { _radius = value; }
+        }
+
+        public int RawRadius
+        {
+            get { return _radius; }
+        }
+
+        public int RawRange
+        {
+            get { return _range; }
         }
 
         public int Range
         {
             get
             {
-                return RawRange +
-                       (Type == SkillShotType.SkillshotLine || Type == SkillShotType.SkillshotMissileLine
+                return _range +
+                       ((Type == SkillShotType.SkillshotLine || Type == SkillShotType.SkillshotMissileLine)
                            ? Config.SkillShotsExtraRange
                            : 0);
             }
-            set { RawRange = value; }
+            set { _range = value; }
         }
-
-        public int RawRadius { get; private set; }
-
-        public int RawRange { get; private set; }
-
-        #endregion
     }
 }
